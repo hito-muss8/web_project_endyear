8a9bc82b486b5d63a6e5d945a5e07f0904e86574
hito mus <mustaqimhito@gmail.com>
2025-01-28 19:45:07 +0700

    cobacoba

--- src/App.jsx
+++ src/App.jsx
@@ -107,11 +107,11 @@ function App() {
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative centre-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-65">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-left">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-centre">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Ingin tau lebih banyak? kunjungi berikut {' '}
               <a href="#" className="font-semibold text-indigo-600">
@@ -120,14 +120,14 @@ function App() {
               </a>
             </div>
           </div>
-          <div className="text-left">
+          <div className="text-centre">
             <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
               Hi! I'm Hito Mustaqim
             </h1>
-            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/5">
               Aku adalah seorang pelajar di Indonesia
             </p>
-            <div className="mt-4 flex items-left justify-left gap-x-6">
+            <div className="mt-4 flex items-centre justify-centre gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -154,6 +154,7 @@ function App() {
         </div>
       </div>
     </div>
+    
 
 
   
